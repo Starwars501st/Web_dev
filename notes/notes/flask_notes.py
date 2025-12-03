66aebd8ed093a9045febe8c700f70341bc4535a7
Starwars501st <eva.briggs@ucas-edu.net>
2025-12-03 13:44:11 -0700

    I have things to do

---
+++ notes/notes/flask_notes.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        return redirect(url_for("user", name=request.form["name"]))
+    return render_template("index.html")
+@app.route("/contact")
+def contact():
+    return"<p> bai <p>"
+
+@app.route("/<name>")
+def user(name):
+    return f"<h1> hello, {name}! </h1>"
+
+if __name__== "__main__":
+    app.run(debug=True)
